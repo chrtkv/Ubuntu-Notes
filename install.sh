2a6989486967abccedb5997358e7a502511e6ff8
Kirill Chertkov <chertkov.k@gmail.com>
2018-03-29 19:17:43 +0300

    test

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# installing yandex browser
+
+# installing yandex.browser
 wget -q -O - https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG | sudo apt-key add -
 
 sudo sh -c 'echo "deb [arch=amd64] http://repo.yandex.ru/yandex-browser/deb beta main" > /etc/apt/sources.list.d/yandex-browser-beta.list'
