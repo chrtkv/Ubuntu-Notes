e1883f2a04dcaeeebc11c07669046c4f14af04f2
Kirill Chertkov <chertkov.k@gmail.com>
2018-03-29 19:15:18 +0300

    vscode instal adding

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# installing yandex browser
 wget -q -O - https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG | sudo apt-key add -
 
 sudo sh -c 'echo "deb [arch=amd64] http://repo.yandex.ru/yandex-browser/deb beta main" > /etc/apt/sources.list.d/yandex-browser-beta.list'
@@ -7,3 +7,10 @@ sudo sh -c 'echo "deb [arch=amd64] http://repo.yandex.ru/yandex-browser/deb beta
 sudo apt update
 
 sudo apt install yandex-browser-beta
+
+# installing vs code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code # or code-insiders
